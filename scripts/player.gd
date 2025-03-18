2747c77018858dfdfd1ec97034d4f416bd95fa32
Xamacardoso <xcardoso1997@gmail.com>
2025-03-18 20:47:43 -0300

    Feat (Minigame): Created base minigame structure. Improved player logic readability

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,50 +2,64 @@ extends CharacterBody2D
 class_name Player
 
 @onready var trajectory : Line2D = $Trajectory;
+@onready var isDoingMinigame: bool = false;
 
 var cursorInRange: bool = false;
 var holdingPlayer: bool = false;
-var timeSinceLastHold: int;
 var launchPowerFactor: float = 10.0;
 
 func _physics_process(delta: float) -> void:
 	if (cursorInRange):
-		if Input.is_action_pressed("hold"):
-			print("segurando");
-			holdingPlayer = true;
+		_manage_hold();
 	
 	
 	# Pegar a distancia e o angulo do mouse
-	var mousePosition = get_global_mouse_position();
-	var myPosition = global_position;
-		
-	var distanceToMouse: float = (abs(mousePosition - myPosition)).length()
-	#print("Distancia para o mouse: " + str(distanceToMouse))
+	var mousePosition: Vector2 = get_global_mouse_position();
+	var myPosition: Vector2 = global_position;
 	
+	var distanceToMouse: float = (abs(mousePosition - myPosition)).length()
 	var angleToMouse : float = rad_to_deg(myPosition.angle_to_point(mousePosition));
+	
 	var reversedAngleToMouse: float = -sign(angleToMouse) * 180 + angleToMouse;
 	var reversedAngleToMouseRad: float = deg_to_rad(reversedAngleToMouse);
+	
 	if (holdingPlayer):
-		trajectory.show();
-		rotation_degrees = reversedAngleToMouse;
-		_update_trajectory(delta, (mousePosition-myPosition))
+		movement_foreshadow(reversedAngleToMouse, myPosition, mousePosition, delta);
 		
 	if (Input.is_action_just_released("hold")):
-		if holdingPlayer:
-			print("soltei aqui");
-			trajectory.hide();
-			var launchVector: Vector2 = Vector2(cos(reversedAngleToMouseRad) * distanceToMouse,
-												sin(reversedAngleToMouseRad) * distanceToMouse);
-			velocity = launchVector * launchPowerFactor;
-			
-		holdingPlayer = false;
-		#if (!$TextureRect.mouse_entered):
-			#cursorInRange = false;
+		_manage_release(distanceToMouse, reversedAngleToMouseRad);
 			
 	if velocity:
 		velocity = lerp(velocity, Vector2.ZERO, 0.169);
 
 	move_and_slide();
+
+func _manage_release(distance, releaseAngle):
+	if holdingPlayer:
+		print("soltei aqui");
+		trajectory.hide();
+		var launchVector: Vector2 = Vector2(cos(releaseAngle) * distance,
+											sin(releaseAngle) * distance);
+		velocity = launchVector * launchPowerFactor;
+			
+	holdingPlayer = false;
+
+func _manage_hold():
+	if Input.is_action_pressed("hold"):
+			print("segurando");
+			holdingPlayer = true;
+
+func movement_foreshadow (
+	_angleToFace: float,
+	_playerPos: Vector2, 
+	_mousePos: Vector2, 
+	delta: float) -> void:
+	if (holdingPlayer):
+		trajectory.show();
+		rotation_degrees = _angleToFace;
+		_update_trajectory(delta, (_mousePos - _playerPos))
+	
+	
 
 func _on_texture_rect_mouse_entered() -> void:
 	print("oi");
